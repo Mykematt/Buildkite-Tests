ba2ad5d00d70e5e10b4f84ac20731265820d67fa
Ola Omoge <olabuildkite@olabuildkite.local>
2025-06-27 22:37:08 -0600

    Fix flaky test to allow builds to pass sometimes
    - Remove guaranteed failure test that was causing all builds to fail
    - Keep only 50% flaky test to demonstrate true flakiness
    - Add debug output to show random value
    - Now some builds will pass (random=0) and some will fail (random=1)
    - Perfect for testing flakiness detection on same commit

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -7,11 +7,8 @@ describe "Simple integers" do
 
   it "flaky test that fails 50% of the time" do
     # This test will fail roughly 50% of the time
-    expect(rand(2)).to eq(0), "This test randomly fails to demonstrate Test Analytics with failures"
-  end
-  
-  it "guaranteed failure test" do
-    # This test always fails - for testing purposes
-    expect(1).to eq(2), "This test always fails to test failure analytics"
+    random_value = rand(2)
+    puts "Random value: #{random_value} (expecting 0 to pass)"
+    expect(random_value).to eq(0), "This test randomly fails to demonstrate Test Analytics with failures"
   end
 end
